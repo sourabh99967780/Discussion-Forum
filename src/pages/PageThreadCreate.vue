22cf25283f71bcecb95fb7a3728bed83d21357cc
Sourabh Kumar <sourabh99967780@gmail.com>
2020-10-29 10:46:46 +0530

    Async data fetch addded

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="forum" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
     <h1>
       Create new thread in <i>{{ forum.name }}</i>
     </h1>
@@ -11,21 +11,27 @@
 <script>
 import ThreadEditor from '@/components/ThreadEditor'
 import { mapActions } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 export default {
   components: {
     ThreadEditor
   },
+
+  mixins: [asyncDataStatus],
+
   props: {
     forumId: {
       type: String,
       required: true
     }
   },
+
   computed: {
     forum() {
       return this.$store.state.forums[this.forumId]
     }
   },
+
   methods: {
     ...mapActions(['createThread', 'fetchForum']),
 
@@ -45,8 +51,11 @@ export default {
       this.$router.push({ name: 'Forum', params: { id: this.forum['.key'] } })
     }
   },
+
   created() {
-    this.fetchForum({ id: this.forumId })
+    this.fetchForum({ id: this.forumId }).then(() => {
+      this.asyncDataStatus_fetched()
+    })
   }
 }
 </script>
--- src/pages/PageThreadEdit.vue
+++ src/pages/PageThreadEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="thread && text" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
     <h1>
       Editing <i>{{ thread.title }}</i>
     </h1>
@@ -16,16 +16,21 @@
 <script>
 import ThreadEditor from '@/components/ThreadEditor'
 import { mapActions } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 export default {
   components: {
     ThreadEditor
   },
+
+  mixins: [asyncDataStatus],
+
   props: {
     id: {
       type: String,
       required: true
     }
   },
+
   computed: {
     thread() {
       return this.$store.state.threads[this.id]
@@ -35,6 +40,7 @@ export default {
       return post ? post.text : null
     }
   },
+
   methods: {
     ...mapActions(['updateThread', 'fetchThread', 'fetchPost']),
 
@@ -51,9 +57,12 @@ export default {
       this.$router.push({ name: 'ThreadShow', params: { id: this.id } })
     }
   },
+
   created() {
     this.fetchThread({ id: this.id }).then(thread =>
-      this.fetchPost({ id: thread.firstPostId })
+      this.fetchPost({ id: thread.firstPostId }).then(() => {
+        this.asyncDataStatus_fetched()
+      })
     )
   }
 }
